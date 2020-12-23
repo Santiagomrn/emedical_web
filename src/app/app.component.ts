75b1ed3d160dfeda67cc8f0303c834ee64e580be
GarciaAlejandro <alejandrogc672@gmail.com>
2020-12-22 18:59:14 -0600

    Login page/buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
+import M from 'materialize-css';
 import { Component } from '@angular/core';
-import { MedicamentService} from "./services/medicament/medicament.service"
-
+import { MedicamentService} from "./services/medicament/medicament.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +8,16 @@ import { MedicamentService} from "./services/medicament/medicament.service"
 })
 export class AppComponent {
   constructor(private MedicamentService: MedicamentService){}
-  
+  login : string;
+  ngOnInit(){
+    this.login = localStorage.getItem("token");
+    console.log("Token : "+this.login);
+  }
   title = 'medical-portal';
+  
+  ngAfterViewInit() {
+      var elems = document.querySelectorAll('.dropdown-trigger');
+      var instances = M.Dropdown.init(elems, {hover: true});
+  }
 }
+
