2f09286d244a1a99df0b15d89473b5e42d441da1
FranciscoPech <fepc087@gmail.com>
2021-01-29 19:42:14 -0500

    casi listo

--- src/app/services/news/news.service.ts
+++ src/app/services/news/news.service.ts
@@ -2,25 +2,40 @@ import { Injectable } from '@angular/core';
 import { NewsInterface } from "../../interfaces/news/news-interface";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+/**
+ * Inyección de angular
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
+   /**
+   * Valor que es un arreglo para realizar la busqueda de noticias y usar promesas
+   */
   cachedValues: Array<{
-    [query: string]: NewsInterface // Hacer referencia a la interface
+    [query: string]: NewsInterface 
   }> = [];
 
+  /**
+   * Constructor que invoca otros modulos
+   * @param {HttpClient} http se utiliza para la autorización del token
+   */
   constructor(private http: HttpClient) {
     this.http = http
   }
 
+  /**
+   * Método para la obtención de noticias que se visualizan en la vista home
+   * @param limit de las noticias a visualizar
+   * @param offset 
+   * @param country de las noticias
+   */
   getNews = (limit: string, offset: string, country: string): Promise<NewsInterface> => {
     let promise = new Promise <NewsInterface>((resolve, reject) => {
       if (this.cachedValues[limit+offset+country]) {
         resolve(this.cachedValues[limit+offset+country])
       } else {
-        //this.http.get('http://api.coronatracker.com/news/trending?limit=' + limit + '&country=' + country + '&offset=' + offset)
         this.http.get('/news'+ 'trending?limit=' + limit + '&country=' + country + '&offset=' + offset)  
         .toPromise()
           .then((response) => {
