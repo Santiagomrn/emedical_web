da21e2db0e2cded7194fac7d148af91d1d9f41d8
FranciscoPech <fepc087@gmail.com>
2021-01-29 19:36:25 -0500

    test

--- src/app/services/doctor/doctor.service.ts
+++ src/app/services/doctor/doctor.service.ts
@@ -2,19 +2,35 @@ import { Injectable } from '@angular/core';
 import { DoctorInterface } from "../../interfaces/doctor/doctor-interface";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-
+/**
+ * Inyección de angular
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
+  /**
+   * Valor que es un arreglo para realizar la busqueda de doctres y usar promesas
+   */
   cachedValues: Array<{
-    [query: string]: DoctorInterface // Hacer referencia a la interface
+    [query: string]: DoctorInterface 
   }> = [];
+  
+   /**
+   * Constructor que invoca otros modulos
+   * @param {HttpClient} http se utiliza para la autorización del token
+   */
   constructor(private http: HttpClient) {
     this.http = http
   }
 
+  /**
+   * Método para la obtención de doctores que se visualizan en la vista de doctores
+   * @param {string} results que son los datos a buscar
+   * @param {string} page en la página que se desea, existen varios
+   * @return la información de los doctores mediante una API
+   */
   getDoctors = (results: string, page: string): Promise<DoctorInterface> => {
     let promise = new Promise <DoctorInterface>((resolve, reject) => {
       if (this.cachedValues[results+page]) {
